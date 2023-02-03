5a2dee93dcf58296851ea1e666133956fe83fb56
Gravetanha <diogo_dpereira@hotmail.com>
2023-02-03 23:05:41 +0000

    Jogo
    Deletei tudo e meti o meu

---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,23 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    public float speed = 10.0f;
+    public float rotationSpeed = 100.0f;
+
+    void Update()
+    {
+        float horizontal = Input.GetAxis("Horizontal");
+        float vertical = Input.GetAxis("Vertical");
+
+        Vector3 movement = new Vector3(horizontal, 0, vertical);
+        if (movement.magnitude > 0.1f)
+        {
+            transform.forward = Vector3.Slerp(transform.forward, movement, Time.deltaTime * rotationSpeed);
+        }
+
+        transform.position += movement * speed * Time.deltaTime;
+    }
+}
