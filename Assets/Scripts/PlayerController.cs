0732c3216394d66f2d9cdc334e1e548fe1adac60
cat <catarina.lopes.04@hotmail.com>
2023-02-04 15:51:00 +0000

    implements new input system

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -1,36 +1,38 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.InputSystem;
 
 public class PlayerController : MonoBehaviour
 {
     public float speed = 10.0f;
     public float currentSpeed;
-    public float rotationSpeed = 100.0f;
+    public float rotationSpeed = 10.0f;
 
+    public Transform hands;
+    private Transform pickedUpObject;
 
+    private Vector2 _movementVector;
 
     private void Start()
     {
         currentSpeed = speed;
+    }    
+
+    private void OnMove(InputValue movementValue)
+    {
+       _movementVector = movementValue.Get<Vector2>();
     }
 
     void Update()
     {
-        float horizontal = Input.GetAxis("Horizontal");
-        float vertical = Input.GetAxis("Vertical");
+        Vector3 movement = new Vector3(_movementVector.x, 0, _movementVector.y);
 
-        Vector3 movement = new Vector3(horizontal, 0, vertical);
         if (movement.magnitude > 0.1f)
         {
             transform.forward = Vector3.Slerp(transform.forward, movement, Time.deltaTime * rotationSpeed);
         }
 
         transform.position += movement * currentSpeed * Time.deltaTime;
-
-
     }
-
-
-
 }
